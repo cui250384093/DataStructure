24b3c52a72e3f0fda5ea60f5b6525fba53152de8
cui250384093 <cui250384093@163.com>
2021-03-04 14:13:12 +0800

    v1

--- src/com/yl/datastructure/LineData.java
+++ src/com/yl/datastructure/LineData.java
@@ -1,9 +1,17 @@
 package com.yl.datastructure;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author candk
  * @Description
  * @date 3/4/21 - 2:08 PM
  */
 public class LineData {
+
+    @Test
+    public void sparseArrayTest() {
+
+    }
+
 }
