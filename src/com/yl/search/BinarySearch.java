c166d6d87353c9436ef5c3887dd0a19a6c4e9ada
cui250384093 <cui250384093@163.com>
2021-03-16 16:45:43 +0800

    v1

--- src/com/yl/search/BinarySearch.java
+++ src/com/yl/search/BinarySearch.java
@@ -1,5 +1,7 @@
 package com.yl.search;
 
+import java.util.ArrayList;
+
 /**
  * @author candk
  * @Description
@@ -9,11 +11,13 @@ public class BinarySearch {
 
     public static void main(String[] args) {
 
-        int[] arr = {1, 8, 10, 89, 1000, 1234};
+        int[] arr = {1, 8, 10, 89, 1000, 1000, 1000, 1234};
 
-        int res = binarySearch(arr, 0, arr.length, 11);
+        int res = binarySearch(arr, 0, arr.length - 1, 1000);
         System.out.println(res);
 
+        ArrayList resList = binarySearch2(arr, 0, arr.length - 1, 1000);
+        System.out.println(resList);
     }
 
     /**
@@ -39,6 +43,56 @@ public class BinarySearch {
             return binarySearch(arr, left, mid - 1, findVal);
         } else {
             return mid;
+        }
+    }
+
+    /**
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @param findVal
+     * @return
+     */
+    public static ArrayList binarySearch2(int[] arr, int left, int right, int findVal) {
+
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+        int midVal = arr[mid];
+
+        if (findVal > midVal) {
+            return binarySearch2(arr, mid + 1, right,findVal);
+        } else if (findVal < midVal) {
+            return binarySearch2(arr, left, mid - 1, findVal);
+        } else {
+
+            ArrayList<Integer> resList = new ArrayList<>();
+
+            //向左寻找
+            int temp = mid - 1;
+            while (true) {
+                if (temp < 0 || arr[temp] != findVal) {
+                    break;
+                }
+                resList.add(temp);
+                temp--;
+            }
+            resList.add(mid);
+
+            //向右寻找
+            temp = mid + 1;
+            while (true) {
+                if (temp > arr.length - 1 || arr[temp] != findVal) {
+                    break;
+                }
+                resList.add(temp);
+                temp++;
+            }
+
+            return resList;
         }
     }
 }
