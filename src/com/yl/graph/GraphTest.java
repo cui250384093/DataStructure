f36329dfaffe82ffd9ee227003b804bbdd7f556b
cui250384093 <cui250384093@163.com>
2021-03-26 15:26:49 +0800

    feat: Graph

--- src/com/yl/graph/GraphTest.java
+++ src/com/yl/graph/GraphTest.java
@@ -12,7 +12,8 @@ public class GraphTest {
     @Test
     public void test1() {
 
-        String[] vertexValue = {"A", "B", "C", "D", "E"};
+//        String[] vertexValue = {"A", "B", "C", "D", "E", "F", "G", "H"};
+        String[] vertexValue = {"1", "2", "3", "4", "5", "6", "7", "8"};
         Graph graph = new Graph(vertexValue.length);
         for (String vertex : vertexValue) {
             graph.insertVertex(vertex);
@@ -20,9 +21,13 @@ public class GraphTest {
 
         graph.insertEdge(0, 1, 1);
         graph.insertEdge(0, 2, 1);
-        graph.insertEdge(1, 2, 1);
         graph.insertEdge(1, 3, 1);
         graph.insertEdge(1, 4, 1);
+        graph.insertEdge(3, 7, 1);
+        graph.insertEdge(4, 7, 1);
+        graph.insertEdge(2, 5, 1);
+        graph.insertEdge(2, 6, 1);
+        graph.insertEdge(5, 6, 1);
 
         graph.dfs();
         System.out.println();
